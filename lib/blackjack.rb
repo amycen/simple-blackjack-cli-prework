c499cb86737bcdf688532d75f6d8132714960c7e
Amy Cen <amy.cen@blackrock.com>
2019-04-13 23:21:26 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -34,8 +34,18 @@ def initial_round
   display_card_total(card_total)
 end
 
-def hit?
+def hit?(card_total)
   # code hit? here
+  prompt_user
+  answer = get_user_input
+  if answer == "h"
+    card_total += deal_card
+  elsif answer == "s"
+    break
+  else
+    invalid_command
+  end
+  card_total
 end
 
 def invalid_command
